d1086f842a5afbabb5bf11118c1d1a3e7f38a2a8
SawaAser <144454570+SawaAser@users.noreply.github.com>
2024-09-10 23:26:06 +0300

    Update settings.py

--- library/library/settings.py
+++ library/library/settings.py
@@ -97,13 +97,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASE = "simpledbmamager"
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        'PASSWORD': '162534',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '162534'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
